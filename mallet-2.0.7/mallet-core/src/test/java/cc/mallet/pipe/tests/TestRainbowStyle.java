220b3d0b521f45f7b4777ec8a5bcf4a4f8c5bea8
Nitin Verma <nitin.matrix@gmail.com>
2013-05-07 14:40:24 +0530

    Fixed #2: Not Functional fix, just setup to create needed dir

--- mallet-2.0.7/mallet-core/src/test/java/cc/mallet/pipe/tests/TestRainbowStyle.java
+++ mallet-2.0.7/mallet-core/src/test/java/cc/mallet/pipe/tests/TestRainbowStyle.java
@@ -6,58 +6,60 @@
    information, see the file `LICENSE' included with this distribution. */
 
 
-
-
-/** 
-   @author Andrew McCallum <a href="mailto:mccallum@cs.umass.edu">mccallum@cs.umass.edu</a>
+/**
+ @author Andrew McCallum <a href="mailto:mccallum@cs.umass.edu">mccallum@cs.umass.edu</a>
  */
 
 package cc.mallet.pipe.tests;
 
-import junit.framework.*;
-import java.net.URI;
-import java.net.URL;
+import cc.mallet.pipe.*;
+import cc.mallet.pipe.iterator.FileIterator;
+import cc.mallet.types.Instance;
+import cc.mallet.types.InstanceList;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
 import java.io.File;
 import java.util.Iterator;
-import java.util.regex.*;
+import java.util.regex.Pattern;
 
-import cc.mallet.pipe.*;
-import cc.mallet.pipe.iterator.*;
-import cc.mallet.types.*;
 
-public class TestRainbowStyle extends TestCase
-{
-	public TestRainbowStyle (String name) {
-		super (name);
-	}
-	
-	public void testThree ()
-	{
-		InstanceList il = new InstanceList (
-			new SerialPipes (new Pipe[] {
-				new Target2Label (),
-				new CharSequence2TokenSequence (),
-				new TokenSequenceLowercase (),
-				new TokenSequenceRemoveStopwords (),
-				new TokenSequence2FeatureSequence (),
-				new FeatureSequence2FeatureVector ()
-			}));
-		Iterator<Instance> pi = new FileIterator (new File("foo/bar"), null, Pattern.compile("^([^/]*)/"));
-		il.addThruPipe (pi);
-	}
+public class TestRainbowStyle extends TestCase {
+    private final static File dir = new File("foo/bar");
 
-	public static Test suite ()
-	{
-		return new TestSuite (TestRainbowStyle.class);
-	}
+    public TestRainbowStyle(String name) {
+        super(name);
+    }
 
-	protected void setUp ()
-	{
-	}
+    protected void setUp() {
+        dir.mkdirs();
+    }
 
-	public static void main (String[] args)
-	{
-		junit.textui.TestRunner.run (suite());
-	}
-	
+    protected void tearDown() {
+        dir.delete();
+    }
+
+    public void testThree() {
+        InstanceList il = new InstanceList(
+                new SerialPipes(new Pipe[]{
+                        new Target2Label(),
+                        new CharSequence2TokenSequence(),
+                        new TokenSequenceLowercase(),
+                        new TokenSequenceRemoveStopwords(),
+                        new TokenSequence2FeatureSequence(),
+                        new FeatureSequence2FeatureVector()
+                }));
+        Iterator<Instance> pi = new FileIterator(dir, null, Pattern.compile("^([^/]*)/"));
+        il.addThruPipe(pi);
+    }
+
+    public static Test suite() {
+        return new TestSuite(TestRainbowStyle.class);
+    }
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
 }
