d20d9ff9eaadcc1e508214e63012a50f4be99083
Nitin Verma <nitin.matrix@gmail.com>
2013-05-08 12:40:04 +0530

    code reformat

--- mallet-2.0.7/mallet-core/src/main/java/cc/mallet/pipe/TokenSequence2FeatureVectorSequence.java
+++ mallet-2.0.7/mallet-core/src/main/java/cc/mallet/pipe/TokenSequence2FeatureVectorSequence.java
@@ -6,86 +6,79 @@
    information, see the file `LICENSE' included with this distribution. */
 
 
-
-
-
 package cc.mallet.pipe;
 
-import java.io.*;
-
 import cc.mallet.types.Alphabet;
-import cc.mallet.types.FeatureVector;
 import cc.mallet.types.FeatureVectorSequence;
 import cc.mallet.types.Instance;
-import cc.mallet.types.Token;
 import cc.mallet.types.TokenSequence;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
 /**
  * Convert the token sequence in the data field of each instance to a feature vector sequence.
-   @author Andrew McCallum <a href="mailto:mccallum@cs.umass.edu">mccallum@cs.umass.edu</a>
+ *
+ * @author Andrew McCallum <a href="mailto:mccallum@cs.umass.edu">mccallum@cs.umass.edu</a>
  */
 
-public class TokenSequence2FeatureVectorSequence extends Pipe implements Serializable
-{
-	boolean augmentable;									// Create AugmentableFeatureVector's in the sequence
-	boolean binary;												// Create binary (Augmentable)FeatureVector's in the sequence
-	boolean growAlphabet = true;
-	
-	public TokenSequence2FeatureVectorSequence (Alphabet dataDict,
-																							boolean binary, boolean augmentable)
-	{
-		super (dataDict, null);
-		this.augmentable = augmentable;
-		this.binary = binary;
-	}
+public class TokenSequence2FeatureVectorSequence extends Pipe implements Serializable {
+    boolean augmentable;                                    // Create AugmentableFeatureVector's in the sequence
+    boolean binary;                                                // Create binary (Augmentable)FeatureVector's in the sequence
+    boolean growAlphabet = true;
 
-	public TokenSequence2FeatureVectorSequence (Alphabet dataDict)
-	{
-		this (dataDict, false, false);
-	}
-	
-	public TokenSequence2FeatureVectorSequence (boolean binary, boolean augmentable)
-	{
-		super (new Alphabet(), null);
-		this.augmentable = augmentable;
-		this.binary = binary;
-	}
+    public TokenSequence2FeatureVectorSequence(Alphabet dataDict,
+                                               boolean binary, boolean augmentable) {
+        super(dataDict, null);
+        this.augmentable = augmentable;
+        this.binary = binary;
+    }
 
-	public TokenSequence2FeatureVectorSequence ()
-	{
-		this (false, false);
-	}
-	
-	public Instance pipe (Instance carrier)
-	{
-		carrier.setData(new FeatureVectorSequence ((Alphabet)getDataAlphabet(),
-																							 (TokenSequence)carrier.getData(),
-																							 binary, augmentable,
-																							 growAlphabet));
-		return carrier;
-	}
+    public TokenSequence2FeatureVectorSequence(Alphabet dataDict) {
+        this(dataDict, false, false);
+    }
 
-	public void setGrowAlphabet(boolean growAlphabet) {
-		this.growAlphabet = growAlphabet;
-	}
+    public TokenSequence2FeatureVectorSequence(boolean binary, boolean augmentable) {
+        super(new Alphabet(), null);
+        this.augmentable = augmentable;
+        this.binary = binary;
+    }
 
-	// Serialization 
-	
-	private static final long serialVersionUID = 1;
-	private static final int CURRENT_SERIAL_VERSION = 0;
-	
-	private void writeObject (ObjectOutputStream out) throws IOException {
-		out.writeInt (CURRENT_SERIAL_VERSION);
-		out.writeBoolean(augmentable);
-		out.writeBoolean(binary);
-		out.writeBoolean(growAlphabet);
-	}
-	
-	private void readObject (ObjectInputStream in) throws IOException, ClassNotFoundException {
-		int version = in.readInt ();
-		augmentable = in.readBoolean();
-		binary = in.readBoolean();
+    public TokenSequence2FeatureVectorSequence() {
+        this(false, false);
+    }
+
+    public Instance pipe(Instance carrier) {
+        carrier.setData(new FeatureVectorSequence((Alphabet) getDataAlphabet(),
+                (TokenSequence) carrier.getData(),
+                binary, augmentable, growAlphabet));
+        return carrier;
+    }
+
+    public void setGrowAlphabet(boolean growAlphabet) {
+        this.growAlphabet = growAlphabet;
+    }
+
+    // Serialization
+
+    private static final long serialVersionUID = 1;
+    private static final int CURRENT_SERIAL_VERSION = 0;
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.writeInt(CURRENT_SERIAL_VERSION);
+        out.writeBoolean(augmentable);
+        out.writeBoolean(binary);
+        out.writeBoolean(growAlphabet);
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        int version = in.readInt();
+        augmentable = in.readBoolean();
+        binary = in.readBoolean();
 //		growAlphabet = true;
-		growAlphabet = in.readBoolean();
-	}
+        growAlphabet = in.readBoolean();
+    }
 
 }
